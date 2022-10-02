f62c5ff17ec3035fa960318316f2461ef21723ac
Jonas Franz <jonas.franz@mailbox.org>
2022-10-02 16:29:14 +0200

    Initial commit

---
+++ src/main/java/com/jonas/bugmenot/BugmenotApplication.java
@@ -0,0 +1,13 @@
+package com.jonas.bugmenot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugmenotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugmenotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonas/bugmenot/BugmenotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonas.bugmenot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugmenotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
